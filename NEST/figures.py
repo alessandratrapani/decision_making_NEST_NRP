0fb574b42e5af2a1beda6111362c8ee266aeb911
ElisaBertone <96536899+ElisaBertone@users.noreply.github.com>
2022-03-07 18:35:17 +0100

    Update figures.py

--- NEST/figures.py
+++ NEST/figures.py
@@ -1,22 +1,22 @@
-#TODO comparison NEST/Brian at 0%, 12.8%, 51.2% raster and activity (Hz)
+#TODO1 comparison NEST/Brian at 0%, 12.8%, 51.2% raster and activity (Hz)
 
-#TODO coint toss inuts --> mean and time integration 
-#TODO comparison NEST/Brian COIN TOSS: raster+activity+decision space
-#TODO n=1000 trials who's winning?+Delta S computation --> as figure3C
+#TODO2 coint toss inuts --> mean and time integration 
+#TODO3 comparison NEST/Brian COIN TOSS: raster+activity+decision space
+#TODO4 n=1000 trials who's winning?+Delta S computation --> as figure3C
 
-#TODO coin toss with 0 variability in the inputs
+#TODO5 coin toss with 0 variability in the inputs
 
-#TODO network performance and error trials Fig4a
-#TODO evaluate the network time courses at: 3.2%, 6.4%,12.8%, 25.6% Fig4b (n trials=1000 and take the mean)
+#TODO6 network performance and error trials Fig4a
+#TODO7 evaluate the network time courses at: 3.2%, 6.4%,12.8%, 25.6% Fig4b (n trials=1000 and take the mean)
 
-#TODO compare 0.0% and 51.2% --> time that it take to cross the 15Hz threshold (figure 5a-b) --> need to find a linear relationship between mean reaction time and log coherence level
+#TODO8 compare 0.0% and 51.2% --> time that it take to cross the 15Hz threshold (figure 5a-b) --> need to find a linear relationship between mean reaction time and log coherence level
 
-#TODO test stimulus duration fig6A
+#TODO9 test stimulus duration fig6A
 
-#TODO test persistent activity --> decrease recurrent exc weights -> again 0 12.8 51.2
+#TODO10 test persistent activity --> decrease recurrent exc weights -> again 0 12.8 51.2
 # da notare: no ramping, no winner takes all a 0.0, no persistent activity.
 
-#TODO testare NMDA slow reverberation --> switch off?
+#TODO11 testare NMDA slow reverberation --> switch off?
 
-#TODO reverse decision --> possibilità di cambiare quando avviene lo stimolo reverse (Percentage choices for A and B as function of the onset time of reversal.Fig8A Even when the signal is reversed 1 s after the stimulus onset, the decision is still re- versable by a more powerful input. Percent- age choices for A and B as function of the coherence level of the reversed signalFig8B
+#TODO12 reverse decision --> possibilità di cambiare quando avviene lo stimolo reverse (Percentage choices for A and B as function of the onset time of reversal.Fig8A Even when the signal is reversed 1 s after the stimulus onset, the decision is still re- versable by a more powerful input. Percent- age choices for A and B as function of the coherence level of the reversed signalFig8B
 #coherence above 70%–80%
