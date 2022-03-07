99239c690f16e7d7b6196f6fa232bfb07195bd31
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2022-03-07 14:40:06 +0100

    script for figures

--- NEST/main.py
+++ NEST/main.py
@@ -22,18 +22,16 @@ current_path = os.getcwd()+'/'
 
 results = simulate_network(coherence, order , start_stim , end_stim , simtime)     
 
+#TODO test EXC without NMDA --> solo AMPA (no recurrent)
 smA = nest.GetStatus(results["spike_monitor_A"])[0]
 rmA = nest.GetStatus(results["rate_monitor_A"])[0]	
 smB = nest.GetStatus(results["spike_monitor_B"])[0]
 rmB = nest.GetStatus(results["rate_monitor_B"])[0]
-
 fig = None
 ax_raster = None
 ax_rate = None
 fig, (ax_raster, ax_rate) = plt.subplots(2, 1, sharex=True, figsize=(16,9))
-
 plt.suptitle('Coherence ' + str(coherence*100) + '%')
-
 evsA = smA["events"]["senders"]
 tsA = smA["events"]["times"]
 ax_raster.plot(tsA, evsA, ".", color='red', label ='pop A')
@@ -43,7 +41,6 @@ ax_raster.plot(tsB, evsB, ".", color='blue', label ='pop B')
 ax_raster.set_ylabel("neuron #")
 ax_raster.set_title("Raster Plot ", fontsize=10)
 ax_raster.legend()
-
 t = np.arange(0., simtime, dt_rec)
 A_N_A = np.ones((t.size, 1)) * np.nan
 trmA = rmA["events"]["times"]
@@ -66,14 +63,11 @@ ax_rate.set_title("Activity", fontsize=10)
 ax_rate.legend()
 plt.xlabel("t [ms]")
 
-
-
 decisional_space = plt.figure(figsize = [10,10])
 if np.mean(A_N_A*1000)>np.mean(B_N_B*1000):
     c='red'
 else:
     c='blue'
-
 plt.plot(A_N_A * 1000,B_N_B * 1000, color=c)
 plt.plot([0,40],[0,40], color='grey')
 plt.xlim(-0.1,40)
@@ -83,3 +77,5 @@ plt.show()
 #TODO save results correctly
 results = pd.DataFrame(results)
 results.to_csv(notes+'results.csv')
+
+#TODO
