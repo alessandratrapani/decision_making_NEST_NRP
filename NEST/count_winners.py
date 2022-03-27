df798c6a6f9b057b1475cb34ccf6ea8e1e74d65c
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2022-03-28 00:11:00 +0200

    debug fig1-2-3-4-5

--- NEST/count_winners.py
+++ NEST/count_winners.py
@@ -2,17 +2,17 @@
 import os
 import pandas as pd
 
+root_path ="C:\\Users\\Alessandra Trapani\\OneDrive - Politecnico di Milano\\"
 
-
-results_main=['standard/','stim_end_500.0/','stim_end_700.0/','stim_end_900.0/','stim_reverse/','t_reverse/']
+results_main=['standard\\','stim_end_500.0\\','stim_end_700.0\\','stim_end_900.0\\','stim_reverse\\','t_reverse\\']
 
 for dt_string in results_main:
-    dirs_results = os.listdir('results/'+dt_string)
+    dirs_results = os.listdir(root_path+'results\\'+dt_string)
     print(dirs_results)
     for n in dirs_results:
-        if os.path.exists('results/'+dt_string+n+'/'):
+        if os.path.exists(root_path+'results\\'+dt_string+n+'\\'):
             win_pop='A_win'
-            path = 'results/'+dt_string+n+'/'+win_pop+ '/'
+            path = root_path+'results\\'+dt_string+n+'\\'+win_pop+ '\\'
             trials = []
             win = []
             if os.path.exists(path):
@@ -22,7 +22,7 @@ for dt_string in results_main:
                     win.append('A')
 
             win_pop='B_win'
-            path =  'results/'+dt_string+n+'/'+win_pop+ '/'
+            path =  root_path+'results\\'+dt_string+n+'\\'+win_pop+ '\\'
             if os.path.exists(path):
                 dirs= os.listdir(path)
                 for i in range(len(dirs)):
@@ -31,4 +31,4 @@ for dt_string in results_main:
             df = {'trial':trials, 'winner':win}
             tryout=pd.DataFrame(df, index=trials)
             tryout = tryout.sort_values(by=['trial'])
-            tryout.to_csv('results/'+dt_string+n +'/'+'trial_winner.csv')
+            tryout.to_csv(root_path+'results\\'+dt_string+n +'\\'+'trial_winner.csv')
